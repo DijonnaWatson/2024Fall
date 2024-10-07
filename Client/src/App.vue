f690254678ce4306a22c1ad92b75e3b9c82d7aae
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-10-07 14:25:06 -0400

    simple formatting

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -8,9 +8,18 @@ import NavBar from './components/NavBar.vue';
     <NavBar />
   </header>
 
+  <div class="container">
   <RouterView />
+  </div>
 </template>
 
 <style scoped>
-
+  .body{
+    background-color: aliceblue;
+  }
+  .container{
+    background-color: whitesmoke;
+    box-shadow: drop-shadow(0 0 5px rgba(0, 0, 0, 0.8));
+    min-height: 100vh;/*is the entire height of your window*/ 
+  }
 </style>
