51911a3ea582e8fff9cda84768cad189cee7a2a4
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-10-07 15:11:58 -0400

    File Base Routing

---
+++ Client/src/pages/Contact/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <form>
+            <div class = "filed">
+                <div class="control">
+               
+        </form>
+    </div>
+</template>
+<style scoped></style>
---
+++ Client/src/pages/Jobs.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <h1 class="title">Jobs</h1>
+        <p>Find your dream job here</p>
+    </div>
+</template>
+<style scoped></style>
