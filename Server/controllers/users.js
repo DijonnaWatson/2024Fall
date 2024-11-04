308fc54b234b905eab5dbc050359457262f2f1ec
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-11-04 15:04:25 -0500

    added Controllers

---
+++ Server/controllers/users.js
@@ -0,0 +1,43 @@
+/*the . means it's one of our files */
+const data = require('../data/users.json');
+const express = require('express');
+const app = express.Router();/*almost axactly the same as the default we get from the express function, */
+
+app.get("/", (req, res) => {
+    //user response obj to send 
+    res.send(data.items)
+})
+
+/*make a locol variable id that points to the */
+.get("/:id", (req, res) => {
+    const id = req.params.id
+    const user = data.items.find(u => u.id === id)/*find calls this function once for every item in items */
+    res.send(user)
+})
+
+.post("/", (req, res) => {
+    const user = req.body
+    user.id = data.items.reduce((prev, x) => (x.id > prev ? x.id: prev),0) + 1
+    data.items.push(user);
+    res.send(user)
+})
+
+.patch("/:id", (req, res) => {
+    const id = req.params.id
+    const user = data.items.find(u => u.id === id)
+    Object.assign(user, req.body)
+    res.send(user)
+})
+.delete("/:id", (req, res) => {
+    const id = req.params.id
+    const userIndex = data.items.findIndex(u => u.id === id)
+    data.items.splice(userIndex, 1);
+    res.send({
+        message: "User deleted successfully"
+    })
+})
+
+/*after added these possible finctions, we need to export them, and will be able to import them in the
+main file */
+
+module.exports = app;/*Common JS way of exporting */
--- Server/index.js
+++ Server/index.js
@@ -3,15 +3,26 @@ function require returns an obj and the obj gets stored in the variable
 since express doesn't start with a "." it will lok for it in the node module folder */
 const express = require("express");
 const app = express();/*create instance of the express pipeline, everytime a message comes in to the port, the operating system gives the message to express and express goes to find out which function it should run*/
+const userController = require("./controllers/users");
+
+
+
 const PORT= 3000;
+
+
+
 /*most basic one is getting without a path*/
 /*registering an action in the pipeline, when a message comes in with the get function and the appl equals "/" which means nothing, than execute this function*/
 /*param one is all info about req, param 2 obj with all methods need to create a most basic response */
 app.get("/", (req, res) => {
   res.send("Hello World");
-});
+})
+    .get("/about", (req, res) => {
+        res.send("About Us");
+    }).use("/users", userController);/**/
 
-/*once pipeline is set up, last thing to do is listen? 
+
+/*This is an Asynchronous function.Once pipeline is set up, last thing to do is listen? 
 anytime see num in the middle of the code its a magin num
 compiler (can't figure out if they're right or if they're wrong)
  and computer don't know what the number means 
